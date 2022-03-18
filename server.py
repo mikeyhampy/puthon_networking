3fe2c27f8e3c203ffb3cdc4707f4828805ea1400
mikeyhampy <90983524+mikeyhampy@users.noreply.github.com>
2022-03-17 23:23:28 -0600

    got server to connect two clients together

---
+++ server.py
@@ -0,0 +1,34 @@
+import socket
+
+# initialize server
+port = 4375
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+serversocket.bind(('0.0.0.0', 22))
+
+
+# listen for people to connect
+while True:
+    clients_to_connect = []
+
+    while True:
+        data, address = serversocket.recvfrom(128)
+
+        print('connection from: {}'.format(address))
+        clients_to_connect.append(address)
+
+        serversocket.sendto(b'ready', address)
+
+        # once there are two people are connected to server, it continues actions
+        if len(clients_to_connect) == 2:
+            print('got 2 clients, sending detaails to each')
+            break
+
+    # removed clients from list
+    c1 = clients_to_connect.pop()
+    c1_addr, c1_port = c1
+    c2 = clients_to_connect.pop()
+    c2_addr, c2_port = c2
+
+    # send client info to each other
+    serversocket.sendto('{} {} {}'.format(c1_addr, c1_port, port).encode(), c2)
+    serversocket.sendto('{} {} {}'.format(c2_addr, c2_port, port).encode(), c1)
