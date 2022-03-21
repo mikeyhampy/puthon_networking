a4c81e44c1cb5d97d647867bd89118c573baed14
mikeyhampy <90983524+mikeyhampy@users.noreply.github.com>
2022-03-21 12:18:53 -0600

    update comments for server and clients

--- client.py
+++ client.py
@@ -3,13 +3,14 @@ import sys
 import threading
 import server_info
 
+# server IP and port
 server_ip = (server_info.SERVER_IP, server_info.SERVER_PORT)
 print('connecting to server')
 
+# conncecting to the server
 clientsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 clientsocket.bind(('0.0.0.0', 4374))
 clientsocket.sendto(b'0', server_ip)
-
 while True:
     data = clientsocket.recv(1024).decode()
 
@@ -17,24 +18,25 @@ while True:
         print('checking in with server, waiting')
         break
 
+# waiting for server to send other client info
 data = clientsocket.recv(1024).decode()
+
+# other client's info
 ip, sport, dport = data.split(' ')
 sport = int(sport)
 dport = int(dport)
 
 print('\ngot peer')
-print('  ip:           {}'.format(ip))
-print('  source port:  {}'.format(sport))
-print('  dest port     {}'.format(dport))
-
 print('punching hole')
 
+# start peer to peer networking
 clientsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 clientsocket.bind(('0.0.0.0', sport))
 clientsocket.sendto(b'0', (ip, dport))
 
 print('ready to exchange messages\n')
 
+# listen for messages to send
 def listen():
     clientsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     clientsocket.bind(('0.0.0.0', sport +2))
@@ -42,12 +44,14 @@ def listen():
     while True:
         data = clientsocket.recv(1024)
         print('\rpeer: {}\n>'.format(data.decode()), end='')
+
+# send messages to peer
 listener = threading.Thread(target=listen, daemon=True)
 listener.start()
-
 clientsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 clientsocket.bind(('0.0.0.0', dport))
 
+# recieve messages from peer
 while True:
     msg = input('> ')
     clientsocket.sendto(msg.encode(), (ip, sport + 2))
